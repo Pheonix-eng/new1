f6bc01fcc425dc6f97691584feea85211b6c5252
Pheonix-eng <labouldry@gmail.com>
2026-04-16 21:09:26 -0600

    Update 10digits.java

--- program.java/10digits.java
+++ program.java/10digits.java
@@ -1,7 +1,7 @@
 public class Program {
     public static void main(String[] args) {
 
-        long start = 1234567890L; // 10-digit number
+        long start = 1234567890L; 
 
         for (int i = 0; i < 100; i++) {
             System.out.println(start + i);
