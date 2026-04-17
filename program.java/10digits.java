360929682732fe7812a5c64816969daeb740d812
Pheonix-eng <labouldry@gmail.com>
2026-04-16 21:08:42 -0600

    Create 10digits.java

---
+++ program.java/10digits.java
@@ -0,0 +1,10 @@
+public class Program {
+    public static void main(String[] args) {
+
+        long start = 1234567890L; // 10-digit number
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(start + i);
+        }
+    }
+}
