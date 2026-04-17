af3e691819f7a01237dd234856b5e346d6077ecd
Natana-Edwards <natana.a.edwards@gmail.com>
2026-04-16 21:43:34 -0600

    Create dummy.java

---
+++ program.java/dummy.java
@@ -0,0 +1,10 @@
+public class Program {
+    public static void main(String[] args) {
+
+        long start = 1234567890L; 
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(start + i);
+        }
+    }
+}
